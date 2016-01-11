e3dd4d563b024dc561209d49e196a00309e66328
ykhorizon <ykhorizon.light@gmail.com>
2016-01-11 22:40:08 +0800

    test svg line,text and animatoin

--- tabproject/static/tabproject/js/test.js
+++ tabproject/static/tabproject/js/test.js
@@ -1,13 +1,38 @@
+// Class TabLine
+// function TabLine(tabWidth, tabWidth, lineNum){
 
+// 	// private
+// 	this.lineNum;
+// 	this.tabWidth = tabWidth;
+// 	this.tabHeight = tabHeight;
+// 	// constructor
+// 	var xmlns = "http://www.w3.org/2000/svg";
+// 	var l = document.createElementNS(xmlns, "line");
+// 	var offset = 45;
 
+// 	l.setAttributeNS(null,"x1",0);
+// 	l.setAttributeNS(null,"y1",offset+(lineNum)*60);
+// 	l.setAttributeNS(null,"x2",tabWidth);
+// 	l.setAttributeNS(null,"y2",offset+(lineNum)*60);
+// 	l.setAttributeNS(null,"style", "stroke:rgb(0,0,0);stroke-width:2");
+// 	this.inti = function(){
+// 		alert(test);
+// 	}
+// }
+
+// writeNote method
+// TabLine.prototype.writeNote = function(fretNum, x, lineNum){
+
+// }
 // tabLine[0] => String 1, other is same
 // tabLines is GLOBAL VARIABL
 tabLines = []; 
 INSTRUMETN_STRING_NUM = 6;
 
 // TODO tabLines become Class 
-// 1.design method for coordinate convertion
-// 2.create contain in tabline for note
+// 1. design method for coordinate convertion
+// 2. create contain in tabline for note
+// 3. testing velocity in 
 
 $(document).ready(function() {
 	
@@ -16,12 +41,12 @@ $(document).ready(function() {
 	var tabWidth = $("#tab").width();
 	var tabHeight = $("#tab").height();
 
-	$("svg").attr("width",tabWidth);
+	$("svg").attr("width",tabWidth*2);
 	$("svg").attr("height",tabHeight);
 	
 
 	for (var i = INSTRUMETN_STRING_NUM - 1; i >= 0; i--) {
-		tabLines[i] = createTabLines(i, tabWidth, tabHeight);		
+		tabLines[i] = createTabLines(i, tabWidth*2, tabHeight);		
 		document.getElementById("tabSVG").appendChild(tabLines[i]);
 	};
 
@@ -29,9 +54,9 @@ $(document).ready(function() {
 	var n = writeNote(15, 30, 1);
 	document.getElementById("tabSVG").appendChild(n);
 
-	// Test Vekicity Packet
-	$("svg text").velocity({ x: tabWidth },
-							{ duration:5000, easing: "linear"});
+	// Test Velocity Packet
+	// $("svg text").velocity({ x: tabWidth },
+	// 						{ duration:5000, easing: "linear"});
 });
 
 function createTabLines(lineNum, tabWidth, tabHeight){
@@ -44,6 +69,7 @@ function createTabLines(lineNum, tabWidth, tabHeight){
 	l.setAttributeNS(null,"x2",tabWidth);
 	l.setAttributeNS(null,"y2",offset+(lineNum)*60);
 	l.setAttributeNS(null,"style", "stroke:rgb(0,0,0);stroke-width:2");
+	
 	return l;
 }
 function writeNote(fretNum, x, lineNum){
