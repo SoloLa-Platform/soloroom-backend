3394434abb0dc1239768ffa5145b42b48bf4e56d
ykhorizon <ykhorizon.light@gmail.com>
2016-03-26 17:42:24 +0800

    add parsing resource, reture json

--- tabproject/views.py
+++ tabproject/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import os
 
 
 def index(request):
 	itemList = ["apple", "banana", "cream"]
 	context = {'itemList' : itemList}
 	return render(request, 'tabproject/index.html', context)
-    # return HttpResponse("Hello, world. You're at the polls index.")
+
+def parsing(request):
+	# if request.method == 'get':
+	rp = os.path.dirname(os.path.abspath(__file__))
+	fp = "{}/parsing/output.json".format(rp)
+	with open(fp, 'r') as file:
+		data = file.read()
+
+	return HttpResponse(data)
